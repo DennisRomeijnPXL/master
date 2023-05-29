98fb5a2371845bcc0a6d8e2e7b483ae986aacb79
DennisRomeijnPXL <73332330+DennisRomeijnPXL@users.noreply.github.com>
2023-05-30 00:45:40 +0200

    Update parsejson.py

--- lab 5 - Software Development and Design Content/parsejson.py
+++ lab 5 - Software Development and Design Content/parsejson.py
@@ -9,3 +9,6 @@ print (ourjson)
 
 print("The access token is: {}".format(ourjson['access_token']))
 print("The token expires in {} seconds.".format(ourjson['expires_in']))
+
+print("\n\n---")
+print(yaml.dump(ourjson))
