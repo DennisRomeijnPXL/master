fd609b04fe0b0544ce3372338dcc9ca58923aa48
DennisRomeijnPXL <73332330+DennisRomeijnPXL@users.noreply.github.com>
2023-05-29 16:16:20 +0200

    Create personal-info.py

---
+++ lab 3 -  Python review - Development tools and classes/personal-info.py
@@ -0,0 +1,6 @@
+firstName = input("What is your first name? ")
+lastname = input("what is your last name? ")
+location = input("what is your location? ")
+age = input("how old are you? ")
+
+print("Hi " + firstname + lastname "! Your location is " + location + " and you are " + age + " years old")
