02d742f88aa51f57a4782b856ebb8dbee2411e14
DennisRomeijnPXL <73332330+DennisRomeijnPXL@users.noreply.github.com>
2023-05-29 12:52:42 +0200

    Update 1.2_geopy-geocoders_location.py

--- Lab 1 - Python Experiments/1.2_geopy-geocoders_location.py
+++ Lab 1 - Python Experiments/1.2_geopy-geocoders_location.py
@@ -1,11 +1,23 @@
 from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="specify_your_app_name_here")
-
-location = geolocator.reverse("52.509669, 13.376294")
-## print location of previous mentioned coordinates
+import folium
+#
+geolocator = Nominatim(user_agent="http://biasc.be")
+#### Enter city and country
+city_country = "Brussels, Belgium"
+####
+location = geolocator.geocode(city_country)
+## print the place (city, country)
 print(location.address)
+devnet_lat = location.latitude
+devnet_lon = location.longitude
 ## print coordinates
-print((location.latitude, location.longitude))
-## show the response of geopy
-print(location.raw)
+print((devnet_lat, devnet_lon))
+#
+coordinates = [devnet_lat,devnet_lon]
+## create map object with this information
+map = folium.Map(location=coordinates, tiles='OpenStreetMap',  zoom_start=12)
+## display interactive map of "map"-object
+map
+# save method of Map object will create a map
+# saved in Downloads
+map.save("geopy_location.html")
